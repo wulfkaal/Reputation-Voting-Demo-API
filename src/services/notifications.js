dae75c0e5a7f59ec9cac42198c84c1f759b77140
dhiraka <sparkx.radhika@gmail.com>
2018-11-26 16:53:02 +0530

    email mandatory for notification instead of userId, api to mark as seen

--- src/services/notifications.js
+++ src/services/notifications.js
@@ -16,7 +16,7 @@ const notifications = {
   },
   getAll: async (req, res) => {
      const collection = req.db.collection("notifications").find({
-      userId: req.params.userId
+      email: req.params.email
      })
       .toArray((err, docs) => {
         res.status(200).send({
@@ -24,8 +24,24 @@ const notifications = {
         });
       })
   },
+  markAsSeen: async (req, res) => {
+      req.db.collection("notifications").updateMany({
+        email: req.params.email,
+        seen: false
+      }, {
+        $set: {seen: true}
+      }, (err, r) => {
+        if (err) {
+          // If it failed, return error
+          res.send("There was a problem updating the information in the database")
+        }
+        else {
+          res.status(200).send(req.body)
+        }
+      })
+  },
   create: async (req, res) => {
-    if (req.body.userId){
+    if (req.body.email){
       req.body.createDate = new Date()
       req.body.seen = false
       req.db.collection('notifications').insertOne(req.body, (err, r) => {
@@ -34,11 +50,11 @@ const notifications = {
           res.send("There was a problem adding the information to the database.")
         }
         else {
-            res.status(200).send(req.body);
+            res.status(200).send(r);
         }
       })
     } else {
-      res.send("userId is mandatory.")
+      res.send("user email is mandatory.")
     }
   },
   update: async (req, res) => {
@@ -53,7 +69,7 @@ const notifications = {
       else {
         res.status(200).send(req.body);
       }
-    });
+    })
   },
   delete: async (req, res) => {
 
